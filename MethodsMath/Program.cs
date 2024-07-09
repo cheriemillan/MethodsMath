5b5526a07593c9f93e80a6d34d790b9f2f82e9ef
CherieMillan <cherie99millan@gmail.com>
2024-07-09 14:36:07 -0400

    Made a program that can add,subtract,divide, and multiply two numbers

---
+++ MethodsMath/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace MethodsMath;
+
+class Program
+{
+    public static int? Add()
+    {
+        int num1=Convert.ToInt32(Console.ReadLine());
+        int num2=Convert.ToInt32(Console.ReadLine());
+        int sum = num1 + num2;
+        Console.WriteLine($"{num1} + {num2} = {sum}");
+        return null;
+    }
+     public static int? Subtract()
+    {
+        int num1=Convert.ToInt32(Console.ReadLine());
+        int num2=Convert.ToInt32(Console.ReadLine());
+        int sum = num1 - num2;
+        Console.WriteLine($"{num1} - {num2} = {sum} ");
+        return null;
+    }
+    public static int? Multiply()
+    {
+        int num1=Convert.ToInt32(Console.ReadLine());
+        int num2=Convert.ToInt32(Console.ReadLine());
+        int sum = num1 * num2;
+        Console.WriteLine($"{num1} * {num2} = {sum} ");
+        return null;
+    }
+    public static int? Divide()
+    {
+        int num1=Convert.ToInt32(Console.ReadLine());
+        int num2=Convert.ToInt32(Console.ReadLine());
+        int sum = num1 / num2;
+        Console.WriteLine($"{num1} / {num2} = {sum} ");
+        return null;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please put in any two numbers of your choosing to add:");
+        Console.WriteLine(Add());
+        Console.WriteLine("Please put in any two numbers of your choosing to subtract:");
+        Console.WriteLine(Subtract());
+        Console.WriteLine("Please put in any two numbers of your choosing to multiply:");
+        Console.WriteLine(Multiply());
+        Console.WriteLine("Please put in any two numbers of your choosing to divide:");
+        Console.WriteLine(Divide());
+    }
+}
